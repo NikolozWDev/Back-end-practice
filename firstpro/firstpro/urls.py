f7019bfca8d8353f0a057b50ff98b053c62bfa6d
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-05-19 00:07:01 +0400

    URL's, Views

--- firstpro/firstpro/urls.py
+++ firstpro/firstpro/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from base.views import home, room
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('room/', room),
 ]
