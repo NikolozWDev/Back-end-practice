3b3ae3a11588780452413eb71a48c44b1fdbb356
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-05-20 00:30:03 +0400

    templates

--- firstpro/base/views.py
+++ firstpro/base/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+    {'id': 1, 'name': 'Room 1'},
+    {'id': 2, 'name': 'Room 2'},
+    {'id': 3, 'name': 'Room 3'},
+]
+
 def home(request):
-    return HttpResponse('Home Page')
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
 def room(request):
-    return HttpResponse('Room Page')
+    room = None
+    for i in rooms:
+        if i['id'] == int(request.GET.get('id')):
+            room = i
+    context = {'room': room}
+    return render(request, 'room.html', context)
